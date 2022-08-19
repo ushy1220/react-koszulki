4243066d16bf5746ae6e8a82292a66ab49e16449
Michał Bielecki <mb9246@gmail.com>
2022-08-20 00:48:28 +0200

    useMemo

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,46 +1,103 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState, useMemo } from 'react';
 
-const Product = props => {
+const Product = ({name, title, basePrice, colors, sizes}) => {
+
+  //Nowe informacje w stanie o kolorze i o rozmiarze, z pierwszą opcją jako domyślna
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+
+  const getPrice = useMemo(() => {
+    return(
+      basePrice + sizes.find((element) => element.name === currentSize).additionalPrice
+    );
+  }, [currentSize, basePrice, sizes]);
+
+  /*   
+  Przycisk dodaj do koszyka ma pokazywać w konsoli co następuje:
+  
+  const displayBasket = () => {
+
+    return (
+      console.log('Name: ', name),
+      console.log('Price: ', getPrice),
+      console.log('Size: ', currentSize),
+      console.log('Color: ', currentColor)
+    );
+  };
+  
+  */
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {getPrice}$</span>
         </header>
+
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {sizes.map(item => (
+                <li key={item.name}>
+                  <button 
+                    type="button" 
+                    className={clsx(item.name === currentSize && styles.active)}
+                    onClick={() => setCurrentSize(item.name)} // () => funkcja pośrednia, żeby odpaliła się dopiero po kliknięciu
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ))} 
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {colors.map(item => (
+                <li key={item}>
+                  <button
+                  type="button"
+                  className={clsx(styles['color' + item[0].toUpperCase() + item.substr(1).toLowerCase()], item === currentColor && styles.active)}
+                  onClick={() => setCurrentColor(item)}
+                  >
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
-          <Button className={styles.button}>
+          <Button 
+          className={styles.button}>
             <span className="fa fa-shopping-cart" />
           </Button>
         </form>
       </div>
     </article>
-  )
+    )
+};
+
+Product.propTypes= {
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.array.isRequired,
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  sizes: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default Product;
+
+/*
+ <Optionform colors={colors} sizes={sizes} currentSize={currentSize} currentColor={currentColor} setCurrentColor={setCurrentColor} setCurrentSize={setCurrentSize} clickHandler={clickHandler} /> 
+*/
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -7,22 +7,13 @@ const Products = () => {
 
   return (
     <section>
-      <Product
-        id={products[0].id}
-        name={products[0].name}
-        title={products[0].title}
-        colors={products[0].colors}
-        sizes={products[0].sizes}
-        basePrice={products[0].basePrice} />
-      <Product
-        id={products[1].id}
-        name={products[1].name}
-        title={products[1].title}
-        colors={products[1].colors}
-        sizes={products[1].sizes}
-        basePrice={products[1].basePrice} />
+
+      {products.map(product => <Product key ={product.id} id={product.id} name={product.name} title={product.title} colors={product.colors} sizes={product.sizes} basePrice={product.basePrice} /> )}
+
     </section>
   );
 };
 
 export default Products;
+
+// {products.map(product => <Product key= {product.id} {...products} />)}
